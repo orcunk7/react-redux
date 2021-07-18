e134749c5a7e95877db77f3cea45181900b2901b
Orçun Karakaya <77879916+orcunk7@users.noreply.github.com>
2021-07-18 07:17:53 +0000

    using nested fetch

--- my-module.js
+++ my-module.js
@@ -3,4 +3,16 @@ import fetch from 'node-fetch';
 
 fetch("https://jsonplaceholder.typicode.com/users")
 .then((data) => data.json())
-.then((user) => console.log(user))
+.then((user) => {
+  console.log("users yüklendi", user);
+
+  fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((data) => data.json())
+  .then((post) => {
+    console.log("post 1 yüklendi",post);
+
+    fetch("https://jsonplaceholder.typicode.com/posts/2")
+    .then((data)=> data.json())
+    .then((data) => console.log("post 2 yüklendi",data))
+  });
+});
